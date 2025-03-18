6faaeaeead8bc59565e602486dc5d25d963a6214
griminir <viktor.elliot.king@gmail.com>
2025-03-18 13:24:54 +0100

    made a job card for the wider version and added panel to both job cards

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,19 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">employer</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Job Title</h3>
+        <p class="text-sm text-gray-400 mt-auto">full time - salary</p>
+    </div>
+
+    <div class="">
+        <x-tag>Tag 1</x-tag>
+        <x-tag>Tag 2</x-tag>
+        <x-tag>Tag 3</x-tag>
+    </div>
+</x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,18 +1,20 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-left text-sm">
         employer
     </div>
 
-    <div class="py-8 font-bold">
-        <h3>Job Title</h3>
-        <p>full time - salary</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Job Title</h3>
+        <p class="text-sm mt-4">full time - salary</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div class="">
-            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">tag</a> <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">tag</a> <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">tag</a>
+            <x-tag>Tag 1</x-tag>
+            <x-tag>Tag 2</x-tag>
+            <x-tag>Tag 3</x-tag>
         </div>
 
-        <img src="http://place-hold.it/42x42" alt="" class="rounded-xl"/>
+        <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>
