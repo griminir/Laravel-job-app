700bee6cdfe930d3b04bc0c8d5e01a07d6d9d0ad
griminir <viktor.elliot.king@gmail.com>
2025-03-19 11:37:35 +0100

    added so job uses the right database info and used a sequence to make it so that half had something and the other half had other attributes

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -7,10 +7,10 @@
 
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">employer</a>
+        <a href="#" class="self-start text-sm text-gray-400"> {{  $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Job Title</h3>
-        <p class="text-sm text-gray-400 mt-auto">full time - salary</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div class="">
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,12 +2,12 @@
 
 <x-panel class="flex flex-col text-center">
     <div class="self-start text-left text-sm">
-        employer
+        {{ $job->employer->name }}
     </div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Job Title</h3>
-        <p class="text-sm mt-4">full time - salary</p>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
