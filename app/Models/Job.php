e810784afa6056ea0c3387d423c42ee8304180a2
griminir <viktor.elliot.king@gmail.com>
2025-03-19 10:55:42 +0100

    added job and tag relationship to make test work (trying out some test driven development work flow)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
@@ -14,6 +15,15 @@ class Job extends Model
     public function employer(): belongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function tag()
+    {
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
 
