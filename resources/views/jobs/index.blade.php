c184629e879b60f794a5090611dafd5d3f481ec0
griminir <viktor.elliot.king@gmail.com>
2025-03-19 11:20:11 +0100

    moved code from welcome and made it compatible with database and models

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -10,9 +10,9 @@
         <section class="pt-10">
             <x-section-heading>Top Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
-                <x-job-card />
-                <x-job-card />
-                <x-job-card />
+                @foreach($jobs as $job)
+                    <x-job-card :job="$job" />
+                @endforeach
             </div>
         </section>
 
@@ -20,27 +20,20 @@
             <x-section-heading>Tags</x-section-heading>
 
             <div class="mt-6 space-x-1">
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
-                <x-tag>Tag 1</x-tag>
+                @foreach($tags as $tag)
+                    <x-tag :tag="$tag" />
+                @endforeach
             </div>
-
         </section>
 
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
 
             <div class="mt-6 space-y-6">
-                <x-job-card-wide />
-                <x-job-card-wide />
-                <x-job-card-wide />
+                @foreach($jobs as $job)
+                    <x-job-card-wide :job="$job" />
+                @endforeach
+
             </div>
         </section>
     </div>
