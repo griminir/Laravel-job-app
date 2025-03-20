1980b8ff286ac197ccea6e32495e5316fe807c2a
griminir <viktor.elliot.king@gmail.com>
2025-03-20 10:22:49 +0100

    set up patch for logging in and registering and logging out

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
+Route:get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::get('/logout', [SessionController::class, 'destroy'])->middleware('auth');
