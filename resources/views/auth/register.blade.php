616a048b8e824faee673b17eaa8fe29a84b0da6d
griminir <viktor.elliot.king@gmail.com>
2025-03-20 10:55:56 +0100

    added padding to layout, added form components and made the auth.register view and created the RegisterUserController

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-page-heading>Register</x-page-heading>
+
+    {{--    enctype adde because of form accepting a file--}}
+    <x-forms.form method="POST" action="/register" enctype="multipart/form-data">
+        <x-forms.input label="Name" name="name"></x-forms.input>
+        <x-forms.input label="Email" name="email" type="email"></x-forms.input>
+        <x-forms.input label="Password" name="password" type="password"></x-forms.input>
+        <x-forms.input label="Confirm Password" name="password_confirmation" type="password"></x-forms.input>
+
+        <x-forms.divider></x-forms.divider>
+
+        <x-forms.input label="Employer Name" name="Employer" ></x-forms.input>
+        <x-forms.input label="Employer Logo" name="logo" type="file"></x-forms.input>
+
+
+        <x-forms.button>Register</x-forms.button>
+    </x-forms.form>
+</x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,7 +13,7 @@
             rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-custom-black text-white font-custom">
+<body class="bg-custom-black text-white font-custom pb-10">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
