8ebb988c281477be957d1f3dacc8811888641edd
griminir <viktor.elliot.king@gmail.com>
2025-03-21 11:36:52 +0100

    added middleware for current routes

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
-Route::get('/register', [RegisterUserController::class, 'create']);
-Route::post('/register', [RegisterUserController::class, 'store']);
-
-Route::get('/login', [SessionController::class, 'create']);
-Route::post('/login', [SessionController::class, 'store']);
-Route::get('/logout', [SessionController::class, 'destroy'])->middleware('auth');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterUserController::class, 'create']);
+    Route::post('/register', [RegisterUserController::class, 'store']);
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
+});
+Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth');
