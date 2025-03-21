7bcf15513322ac0f91cbf12efd5bcda6dadbf9d5
griminir <viktor.elliot.king@gmail.com>
2025-03-21 11:38:14 +0100

    made controller for logging in logging out and navigating to log in view

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        if (RateLimiter::tooManyAttempts(
+            key: 'login:'.request()->input('email'), maxAttempts: 5)) {
+            throw ValidationException::withMessages([
+                'email' => 'Too many login attempts. Please try again later.',
+            ]);
+        }
+
+        $attributes = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(! Auth::attempt($attributes, true)){
+            RateLimiter::hit(key: 'login:'.request()->input('email'), decaySeconds: 120);
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        RateLimiter::clear(key: 'login:'.request()->input('email'));
+
+        return redirect('/')->with('success', 'Welcome back!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Auth::logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
