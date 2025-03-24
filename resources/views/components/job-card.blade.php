150721ae5ff0c5a1ad6ca9013239a067ca985503
griminir <viktor.elliot.king@gmail.com>
2025-03-24 12:12:12 +0100

    added url for the job and added employer prop so it can find the logo

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,12 +2,17 @@
 
 <x-panel class="flex flex-col text-center">
     <div class="self-start text-left text-sm">
-        {{ $job->employer->name }}
+            {{ $job->employer->name }}
     </div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
-        <p class="text-sm mt-4">{{ $job->salary }}</p>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}</h3>
+            </a>
+        <p class="text-sm mt-4">
+                {{ $job->salary }}
+        </p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
@@ -17,6 +22,6 @@
             @endforeach
         </div>
 
-        <x-employer-logo :width="42"/>
+        <x-employer-logo :employer="$job->employer" :width="42"/>
     </div>
 </x-panel>
