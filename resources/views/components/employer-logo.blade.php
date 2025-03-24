bccabd344564150c185d455497e65ffc649dab43
griminir <viktor.elliot.king@gmail.com>
2025-03-24 12:27:22 +0100

    simplified alt msg for logo

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,6 +1,6 @@
 @props(['employer', 'width' => 90])
 
 {{--had to add a asset_url to .env file for url/storage to make asset work--}}
-<img src="{{ asset($employer->logo) }}" alt="{{ $employer->name }} logo" width="{{ $width }}" class="rounded-xl"/>
+<img src="{{ asset($employer->logo) }}" alt="logo" width="{{ $width }}" class="rounded-xl"/>
 
 {{-- http://picsum.photos/seed/{{ rand(0,1000) }}/{{ $width }}--}}
